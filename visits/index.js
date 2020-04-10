16b48d4280c54b9c03f581dc1338087f9b17aea3
Asim Amalani <asim.amalani@gmail.com>
2020-04-10 17:43:39 -0400

    increment site visits. docker compose configuration for running node app and redis containers to communicate with each other

--- visits/index.js
+++ visits/index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const redis = require('redis');
 const app = express();
 
-const client = redis.createClient();
-client.set('visits', '0');
+const client = redis.createClient({
+    host: 'redis-server'
+});
+client.set('visits', 0);
 
-app.get('/', (req, res) => {
-    client.get('visits', (err, visists) => {
+app.get('/', (_req, res) => {
+    client.get('visits', (err, visits) => {
         if (err) {
             res.send('Something went wrong. Please try again later...');
             return;
         }
-        res.send(`Number of visits: ${visists}`);
+        res.send(`Number of visits: ${visits}`);
+        client.set('visits', parseInt(visits) + 1);
     });
 });
 
